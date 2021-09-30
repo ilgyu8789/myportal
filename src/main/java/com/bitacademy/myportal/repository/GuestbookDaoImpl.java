951e6d136f8535517632a3bcd503b2d6a34cca78
박일규 <t5nn123@naver.com>
2021-09-30 11:15:11 +0900

    방명록 서비스 연결

---
+++ src/main/java/com/bitacademy/myportal/repository/GuestbookDaoImpl.java
@@ -0,0 +1,30 @@
+package com.bitacademy.myportal.repository;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class GuestbookDaoImpl implements GuestbookDao {
+	@Autowired
+	SqlSession sqlSession;
+	
+	@Override
+	public List<GuestbookVo> selectAll() {
+		List<GuestbookVo> list = sqlSession.selectList("guestbook.selectAll");
+		return list;
+	}
+
+	@Override
+	public int insert(GuestbookVo vo) {
+		return 0;
+	}
+
+	@Override
+	public int delete(GuestbookVo vo) {
+		return 0;
+	}
+
+}
